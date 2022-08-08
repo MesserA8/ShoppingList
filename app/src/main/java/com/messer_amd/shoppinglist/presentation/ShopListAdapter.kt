1d12c3eb995bf30a8ec647570525926b8d6815bd
MesserA8 <atihon09@gmail.com>
2022-08-08 12:12:14 +0300

    ShopListAdapter, ViewHolder complete

--- app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,8 +1,11 @@
 package com.messer_amd.shoppinglist.presentation
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.messer_amd.shoppinglist.R
 import com.messer_amd.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
@@ -10,16 +13,26 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     val list = listOf<ShopItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.view.setOnLongClickListener {
+            true
+        }
+
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ShopItemViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
 }
