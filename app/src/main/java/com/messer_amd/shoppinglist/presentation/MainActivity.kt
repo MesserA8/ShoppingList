c450c2917fc2d1f3cc8cec1ac3034eb4c519e9d4
MesserA8 <atihon09@gmail.com>
2022-08-09 11:17:57 +0300

    fun showList deleted

--- app/src/main/java/com/messer_amd/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/messer_amd/shoppinglist/presentation/MainActivity.kt
@@ -13,37 +13,13 @@ import com.messer_amd.shoppinglist.domain.ShopItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var llShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)//[MainViewModel::cl.java]
         viewModel.shopList.observe(this) {
-            showList(it)
 
-        }
-    }
-
-    private fun showList(list: List<ShopItem>) {
-        llShopList.removeAllViews()
-        for (shopItem in list) {
-    val layoutId = if (shopItem.enabled) {
-        R.layout.item_shop_enabled
-    }else{
-        R.layout.item_shop_disabled
-    }
-            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = shopItem.name
-            tvCount.text = shopItem.count.toString()
-            view.setOnLongClickListener {
-                viewModel.changeEnableState(shopItem)
-                true
-            }
-            llShopList.addView(view)
         }
     }
 }
