7f8b2b751058398b9fe66b7da2430dae1c981656
MesserA8 <atihon09@gmail.com>
2022-08-15 13:19:30 +0300

    fun addShopItem is ready

--- app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemViewModel.kt
@@ -19,11 +19,42 @@ class ShopItemViewModel: ViewModel() {
         val item = getShopItemUseCase.getShopItem(shopItemId)
     }
 
-    fun addShopItem(shopItem: ShopItem) {
-        addShopItemUseCase.addShopItem(shopItem)
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
     }
 
     fun editShopItem(shopItem: ShopItem) {
         editShopItemUseCase.editShopItem(shopItem)
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            // TODO: show error input name
+             result = false
+        }
+        if (count <= 0) {
+            // TODO: show error input count
+            result = false
+        }
+        return result
     }
 }
