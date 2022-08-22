58b0fc8f381ab9efa54fb120ef1558d0e26a7903
MesserA8 <atihon09@gmail.com>
2022-08-22 09:16:31 +0300

    initial view in ShopItemActivity.kt

--- app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemActivity.kt
@@ -4,14 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.messer_amd.shoppinglist.R
 
-class ShopItemActivity: AppCompatActivity() {
+class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var buttonSave: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        initViews()
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+    }
+
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        buttonSave = findViewById(R.id.save_button)
     }
 
     companion object {
