fa28de3098c9ff84445ed98f38411abef91aecc6
MesserA8 <atihon09@gmail.com>
2022-08-22 09:44:02 +0300

    fun parseIntent for RunTimeException added

--- app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/messer_amd/shoppinglist/presentation/ShopItemActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputLayout
 import com.messer_amd.shoppinglist.R
+import com.messer_amd.shoppinglist.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
 
@@ -21,12 +22,35 @@ class ShopItemActivity : AppCompatActivity() {
     private lateinit var etCount: EditText
     private lateinit var buttonSave: Button
 
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        parseIntent()
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
         initViews()
+
+    }
+
+    // check intent parameters ->
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+          if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+              throw RuntimeException("Param shop item id is absent")
+          }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
     }
 
     private fun initViews() {
@@ -43,6 +67,7 @@ class ShopItemActivity : AppCompatActivity() {
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
 
         fun newIntentAddItem(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
