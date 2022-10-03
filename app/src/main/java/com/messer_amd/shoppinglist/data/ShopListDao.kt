d1f42edb5098d1bb471e58205f830550ec9a1088
MesserA8 <atihon09@gmail.com>
2022-10-03 10:52:36 +0300

    interface Dao added

---
+++ app/src/main/java/com/messer_amd/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,23 @@
+package com.messer_amd.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModel>> //метод возвращает список всех записей
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItem(shopItemDbModel: ShopItemDbModel) // метод добавляет новую запись в таблицу
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItem(shopItemId: Int)
+
+    @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int): ShopItemDbModel
+}
