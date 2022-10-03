9a9b16d34545abc75b1a3aabf64d0dc01a596743
MesserA8 <atihon09@gmail.com>
2022-10-03 10:29:13 +0300

    model data base added

---
+++ app/src/main/java/com/messer_amd/shoppinglist/data/ShopItemDbModel.kt
@@ -0,0 +1,14 @@
+package com.messer_amd.shoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.messer_amd.shoppinglist.domain.ShopItem
+
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean,
+)
